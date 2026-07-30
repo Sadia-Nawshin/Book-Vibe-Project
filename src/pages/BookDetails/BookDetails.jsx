5b295d8198ff7824f77eb35b22693806502ed10f
Sadia Nawshin <sadianawshin5@gmail.com>
2026-07-30 10:48:14 +0600

    Book Details Page Completed

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -6,12 +6,43 @@ const BookDetails = () => {
     const bookId = parseInt(id);
     const data = useLoaderData();
     const singleBook = data.find(book=> book.bookId === bookId);
-    const {bookName, image} = singleBook;
+    console.log(singleBook);
+    const {bookName, image, author, category, review, tags, publisher, totalPages, yearOfPublishing, rating} = singleBook;
+    
     return (
-        <div className='w-2/3 mx-auto'>
+        <div className='mx-auto flex'>
+            <div className='bg-gray-100 w-1050 h-150 m-5 rounded-[8px] mx-auto flex justify-center items-center'>
+                <img src={image} className='w-52 h-full object-contain' alt="" />
+            </div>
+           
+            <div className='m-5'>
+                <h1 className='font-bold text-2xl'>{bookName}</h1>
+                <p className='font-normal mt-2 pt-2 pb-2'>By : {author}</p>
+
+                <p className='mt-2 mb-2 border-t-1 border-b-1 pt-2 pb-2'>{category}</p>
+
+                <p><span className='font-semibold'>Review :</span> {review}</p>
+
+                <div className='flex flex-wrap items-center font-semibold gap-2 mb-2'>
+                    <span>Tag :</span>
+                    {
+                    tags.map(tag => <p className='text-green-600 font-semibold m-2 p-2 rounded-[8px] bg-green-50'>#{tag}</p>)
+                    }
+                
+                </div>
+
+                <div className='pt-2 pb-2 border-t-1'>
+                    <p className='pt-2 pb-2'>Number of Pages: <span className='font-semibold'>{totalPages}</span></p>
+                    <p className='pt-2 pb-2'>Publisher: <span className='font-semibold'>{publisher}</span></p>
+                    <p className='pt-2 pb-2'>Year of Publishing: <span className='font-semibold'>{yearOfPublishing}</span></p>
+                    <p className='pt-2 pb-2'>Rating: <span className='font-semibold'>{rating}</span></p>
+                </div>
+
+
+                <button className='btn btn-info m-2'>Read</button>
+                <button className='btn btn-accent m-2'>WishList</button>
+            </div>
             
-            <img src={image} className='w-48' alt="" />
-            <h1>{bookName}</h1>
         </div>
     );
 };
